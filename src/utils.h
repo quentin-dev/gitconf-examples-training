c3b8aaa8a8ed5557ace0bc0f9bb8d3d05760a404
Quentin Barbarat <quentin.barbarat@epita.fr>
2020-11-16 12:13:27 +0100

    src: utils.h: add doxygen documentation

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,12 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+/// @file utils.h
+/// @brief Utility functions for our examples
+/// @author Quentin Barbarat
+
+/// @brief Says hello to the user on stdut
+///
 void hello(void);
 
 #endif /* ! UTILS_H */
